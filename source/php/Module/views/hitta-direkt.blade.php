a9ab11fc2c9ed4958d42711b41810718a18b4da5
Jonas Näsman <jonas.nasman@consid.se>
2026-03-09 15:36:53 +0100

    Rename classes

--- source/php/Module/views/hitta-direkt.blade.php
+++ source/php/Module/views/hitta-direkt.blade.php
@@ -1,10 +1,10 @@
 @if (!empty($items))
-    <div class="hitta-direkt" style="--hitta-direkt-columns: {{ (int) $columns }};">
+    <div class="shortcuts" style="--shortcuts-columns: {{ (int) $columns }};">
         @foreach ($items as $item)
             @php($target = $item['link']['target'] ?? '')
             @php($rel = $target === '_blank' ? 'noopener noreferrer' : '')
             <a
-                class="hitta-direkt__item"
+                class="shortcuts__item"
                 href="{{ $item['link']['url'] }}"
                 @if (!empty($target))
                     target="{{ $target }}"
@@ -13,15 +13,15 @@
                     rel="{{ $rel }}"
                 @endif
             >
-                <span class="hitta-direkt__circle" style="--hitta-direkt-color: {{ $item['colorValue'] }};">
+                <span class="shortcuts__circle" style="--shortcuts-color: {{ $item['colorValue'] }};">
                     <span
-                        class="c-icon material-symbols hitta-direkt__icon"
+                        class="c-icon material-symbols shortcuts__icon"
                         data-material-symbol="{{ $item['icon'] }}"
                         aria-hidden="true"
                     ></span>
                 </span>
                 @if ($item['label'] !== '')
-                    <span class="hitta-direkt__label">{{ $item['label'] }}</span>
+                    <span class="shortcuts__label">{{ $item['label'] }}</span>
                 @endif
             </a>
         @endforeach
